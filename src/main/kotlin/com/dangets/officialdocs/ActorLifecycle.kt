378f7264b54098660f47a33a3934c416055c2011
dg <dangets@gmail.com>
2019-03-05 20:43:20 -0700

    added some basic examples

---
+++ src/main/kotlin/com/dangets/officialdocs/ActorLifecycle.kt
@@ -0,0 +1,46 @@
+package com.dangets.officialdocs
+
+import akka.actor.AbstractActor
+import akka.actor.ActorRef
+import akka.actor.ActorSystem
+import akka.actor.Props
+
+class StartStopActor(private val count: Int) : AbstractActor() {
+    private val name = "Actor$count"
+
+    override fun preStart() {
+        println("$name - preStart")
+        if (count > 0)
+            context.actorOf(StartStopActor.props(count - 1))
+    }
+
+    override fun postStop() {
+        println("$name - postStop")
+    }
+
+    override fun createReceive(): Receive {
+        return receiveBuilder()
+            .matchEquals("stop") {
+                context.stop(self)
+            }
+            .build()
+    }
+
+    companion object {
+        fun props(count: Int): Props = Props.create(StartStopActor::class.java, count)
+    }
+}
+
+fun main() {
+    val system = ActorSystem.create("testSystem")
+
+    val a1 = system.actorOf(StartStopActor.props(1))
+    a1.tell("stop", ActorRef.noSender())
+
+    println(">>> Press ENTER to exit <<<")
+    try {
+        readLine()
+    } finally {
+        system.terminate()
+    }
+}
---
+++ src/main/kotlin/com/dangets/officialdocs/SimpleSupervision.kt
@@ -0,0 +1,61 @@
+package com.dangets.officialdocs
+
+import akka.actor.AbstractActor
+import akka.actor.ActorRef
+import akka.actor.ActorSystem
+import akka.actor.Props
+
+class SupervisingActor : AbstractActor() {
+    private val child = context.actorOf(SupervisedActor.props, "supervised-actor")
+
+    override fun createReceive(): Receive {
+        return receiveBuilder()
+            .matchEquals("failChild") {
+                child.tell("fail", self)
+            }
+            .build()
+    }
+
+    companion object {
+        val props: Props = Props.create(SupervisingActor::class.java)
+    }
+}
+
+class SupervisedActor : AbstractActor() {
+    override fun preStart() {
+        println("supervised actor started")
+    }
+
+    override fun createReceive(): Receive {
+        return receiveBuilder()
+            .matchEquals("fail") {
+                println("supervised actor failing now ...")
+                throw IllegalStateException("all is lost")
+            }
+            .build()
+    }
+
+    override fun postStop() {
+        println("supervised actor stopped")
+    }
+
+    companion object {
+        val props: Props = Props.create(SupervisedActor::class.java)
+    }
+}
+
+
+fun main() {
+    val system = ActorSystem.create("testSystem")
+
+    val supervisor = system.actorOf(SupervisingActor.props)
+    supervisor.tell("failChild", ActorRef.noSender())
+
+    println(">>> Press ENTER to exit <<<")
+    try {
+        readLine()
+    } finally {
+        system.terminate()
+    }
+}
+
